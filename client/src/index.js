ee8ec87f39ea77edf03d9e24b160d8a47f2762bd
Lucas Luzzani <90403563+Luzzani@users.noreply.github.com>
2022-10-18 22:45:45 -0300

    Update index.js
    cambio

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import dotenv from 'dotenv'
 
 import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
+dotenv.config();
+
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
